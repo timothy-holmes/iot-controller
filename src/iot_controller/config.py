847ce23665c40168ecc51b22531887316f1c0e1f
Timothy Holmes <tim.a.holmes@gmail.com>
2022-09-03 23:22:48 +0000

    fixed directory typo

--- src/iot_controller/config.py
+++ src/iot_controller/config.py
@@ -3,4 +3,4 @@ from ..logger import build_logger
 
 log = build_logger(".".join(["iot_controller", __name__]))
 
-config: Box = Box.from_json(filename="/config/config.json")
+config: Box = Box.from_json(filename="./config/config.json")
