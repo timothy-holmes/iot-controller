646939b17668fdedbe458eef10860074e4491b77
Timothy Holmes <tim.a.holmes@gmail.com>
2022-09-03 23:27:53 +0000

    changed  reference in main()

--- src/iot_controller/__init__.py
+++ src/iot_controller/__init__.py
@@ -1,11 +1,14 @@
 __version__ = "0.1.0"
 
 import uvicorn
-from .routes import app
 from .config import config
 
 
 def main():
     uvicorn.run(
-        app, host="0.0.0.0", port=config.UVICORN_PORT, log_level="debug", reload=True
+        "src.iot_controller:app",
+        host="0.0.0.0",
+        port=config.UVICORN_PORT,
+        log_level="debug",
+        reload=True,
     )
