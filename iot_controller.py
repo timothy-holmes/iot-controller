198a8e254f6aeba24f73ca74e0d26bf4ccbd76f5
Timothy Holmes <tim.a.holmes@gmail.com>
2022-09-03 23:45:52 +0000

    modified structure

--- iot_controller.py
+++ iot_controller.py
@@ -1,4 +1,11 @@
-from src.iot_controller import main
+import uvicorn
+from src.iot_controller.config import config
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "src.iot_controller:app",
+        host="0.0.0.0",
+        port=config.UVICORN_PORT,
+        log_level="debug",
+        reload=True,
+    )
