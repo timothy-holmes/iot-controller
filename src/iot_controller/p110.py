87bd46749a41a1977144bc8c431038dd7cdcd85c
Timothy Holmes <tim.a.holmes@gmail.com>
2022-10-18 10:59:13 +1100

    fixed timeout error fix

--- src/iot_controller/p110.py
+++ src/iot_controller/p110.py
@@ -6,21 +6,23 @@ from ..logger import log
 
 class P110Controller:
     def __init__(self, config: Box):
-        self.obj = PyP110.P110(
-            config.DEVICE_IP, config.DEVICE_USER, config.DEVICE_PASSWORD
-        )
-        log.info(f"P110 ({config.DEVICE_IP}) controller created: {self.obj=}")
-        a = self.authenticate()
+        self.config = config
+        a = self._authenticate()
         if a:
             log.info(f"P110 ({config.DEVICE_IP}) controller authenticated")
 
-    def authenticate(self):
+    def _authenticate(self):
         try:
             _ = self.obj.getDeviceInfo()
         except Exception as e1:
             e = f"P110 Connection Error: {e1}"
             log.error(e)
             try:
+                self.obj = PyP110.P110(
+                    self.config.DEVICE_IP, 
+                    self.config.DEVICE_USER, 
+                    self.config.DEVICE_PASSWORD
+                )
                 self.obj.handshake()
                 self.obj.login()
             except Exception as e2:
@@ -37,12 +39,12 @@ class P110Controller:
             action_method = getattr(self, '_' + action_str)
             if callable(action_method):
                 try:
-                    result = action_method()
+                    action_method()
                 except Exception as e1:
                     e = f"P110 Connection Error: {e1}"
                     log.error(e)
-                    _ = self.authenticate()
-                    _ = action_method()
+                    self._authenticate()
+                    action_method()
                 return action_str
             else:
                 return 'Invalid method (not callable)'
